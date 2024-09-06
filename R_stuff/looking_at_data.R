fdfa78599f09d4b762c7ca39f4eb75877f63906a
Tim2othy <150283209+Tim2othy@users.noreply.github.com>
2024-09-06 19:39:46 +0200

    analyzing data more, created tree, visualization and most important variables

--- R_stuff/looking_at_data.R
+++ R_stuff/looking_at_data.R
@@ -1,9 +1,50 @@
 # Data-analysis
+install.packages('...')
+library(rpart)
+library(rpart.plot)
+library(dplyr)
+library(ggplot2)
+library(data.tree) 
+library(networkD3)
 
-sd = student_por
+
+sd <- student_por
+sd$G2 <- NULL
+sd$G1 <- NULL
 
 View(sd)
 
 
-plot(sd$G1, sd$G3, col = "blue", pch = 19)
+plot(sd$age, sd$G3, col = "blue", pch = 19)
 grid()
+
+
+
+
+
+tree_model <- rpart(G3 ~ ., data = sd)
+
+
+# Visualize the tree
+rpart.plot(tree_model, shadow.col = "gray")
+rpart.plot(tree_model, extra = 101, fallen.leaves = TRUE, type = 4, main = "Decision Tree Visualization")
+
+# Get variable importance
+var_importance <- tree_model$variable.importance
+print(var_importance)
+
+.C
+dq+u~!!
+
+
+
+# Variable Importance Plot
+var_importance <- data.frame(
+  variable = names(tree_model$variable.importance),
+  importance = tree_model$variable.importance
+)
+ggplot(var_importance, aes(x = reorder(variable, importance), y = importance)) +
+  geom_bar(stat = "identity", fill = "skyblue") +
+  coord_flip() +
+  theme_minimal() +
+  labs(title = "Variable Importance", x = "Variables", y = "Importance")
